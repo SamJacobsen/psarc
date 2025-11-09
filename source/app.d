1e0069ddd7698ae00e613e189d32b558be1666e1
Sam Jacobsen <popblocks66@gmail.com>
2025-11-08 21:21:46 -0500

    Add mode opt

--- source/app.d
+++ source/app.d
@@ -7,49 +7,73 @@ import std.uni : toLower;
 import std.exception : enforce;
 import std.digest.md : md5Of;
 
-void main(string[] args) {
-	string inpt;
-	bool list;
+enum Mode {
+	unpack,
+	list,
+	pack
+}
 
-	getopt(args,
-		std.getopt.config.required,
-		"file|f", "Input file", &inpt,
-		"list|l", "List files", &list);
-
+void list(string inpt) {
 	enforce(toLower(extension(inpt)) == ".psarc", "Unexpected extension " ~ extension(inpt));
-
 	ubyte[] buffer = cast(ubyte[]) read(inpt);
 	PSARC arc = PSARC(buffer);
 
-	if(list) {
-		foreach (string name; arc.names) {
-			writeln(name);
-		}
-		return;
-	}
+	write(cast(char[])arc.readEntry(buffer, 0));
+}
+
+void unpack(string inpt) {
+	enforce(toLower(extension(inpt)) == ".psarc", "Unexpected extension " ~ extension(inpt));
+	ubyte[] buffer = cast(ubyte[]) read(inpt);
+	PSARC arc = PSARC(buffer);
 
 	string arcName = baseName(stripExtension(inpt));
 
 	for (int i = 1; i < arc.tocEntryCount; i++) {
 		enforce(md5Of(arc.names[i - 1]) == arc.entries[i].nameDigestMD5, "MD5 mismatch");
 		string path = arc.names[i - 1];
-		if(arc.flags & ArchiveFlags.relative) {
-		} 
-		
-		if(arc.flags & ArchiveFlags.ignorecase) {
+		if (arc.flags & ArchiveFlags.relative) {
+		}
+
+		if (arc.flags & ArchiveFlags.ignorecase) {
 			path = path.toLower();
 			arcName = arcName.toLower();
-		} 
-		
-		if(arc.flags & ArchiveFlags.absolute) {
-			path = path[1..$];
+		}
+
+		if (arc.flags & ArchiveFlags.absolute) {
+			path = path[1 .. $];
 		}
 
 		path = buildPath(arcName, path);
-		
+
 		if (!exists(dirName(path))) {
 			mkdirRecurse(dirName(path));
 		}
 		std.file.write(path, arc.readEntry(buffer, i));
+	}
+}
+
+void main(string[] args) {
+	Mode mode;
+	string inpt;
+
+	getopt(args,
+		"mode|m", &mode,
+		std.getopt.config.required,
+		"i", &inpt
+	);
+
+	switch (mode) {
+	case mode.pack:
+		writeln(mode.pack);
+		break;
+
+	case mode.unpack:
+		unpack(inpt);
+		break;
+
+	case mode.list:
+		list(inpt);
+		break;
+	default:
 	}
 }
